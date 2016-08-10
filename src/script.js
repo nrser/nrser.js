4698b936f6101de892364c6d89e1bc4f02113f99
nrser <neil@ztkae.com>
2016-08-11 04:06:39 +0800

    sketching out some cmd line utils

--- src/script.js
+++ src/script.js
@@ -1,32 +1,9 @@
 import shellescape from 'shell-escape';
 import _ from 'lodash';
 import { execSync as sysExecSync } from 'child_process';
+import util from 'util';
 
-import { squish, tag } from './string.js';
-
-/**
-* string tag to shell escape interpolations.
-* 
-* @type {function(strings:string[], ...values):string}
-*/
-const shTag = tag(string => shellescape([string]));
-
-/**
-* string template tag to shell escape values.
-*
-* @see https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Template_literals
-* 
-* @param {string[]} strings
-*   array of string literals in template.
-* 
-* @param {...object} values
-*   values to interpolate.
-* 
-* @return {string} shell escaped string.
-*/
-export function sh(strings, ...values) {
-  return squish(shTag(strings, ...values));
-}
+import { squish, tag, deindent } from './string.js';
 
 export function chdir(dest, block) {
   if (block) {
@@ -51,4 +28,82 @@ export function execSync(cmd, options = {encoding: 'utf8', trim: true}) {
   } else {
     return output;
   }
+}
+
+// tags
+// ====
+
+/**
+* string tag to shell escape interpolations.
+* 
+* @type {function(strings:string[], ...values):string}
+*/
+const escTag = tag(string => shellescape([string]));
+
+/**
+* string template tag to shell escape values.
+*
+* @see https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Template_literals
+* 
+* @param {string[]} strings
+*   array of string literals in template.
+* 
+* @param {...object} values
+*   values to interpolate.
+* 
+* @return {string} shell escaped string.
+*/
+export function esc(strings, ...values) {
+  return squish(escTag(strings, ...values));
+}
+
+// old name
+export const sh = esc;
+
+const putsTag = tag(s => _.isString(s) ? s : util.inspect(s));
+
+export function puts(strings, ...values) {
+  console.log(deindent(putsTag(strings, ...values)));
+}
+
+export const p = puts;
+
+// kexec(...args) {
+//   const kexec = require('kexec');
+//   
+//   kexec(sh(...args));
+// }
+
+export class Cmd {
+  constructor(options = {}) {
+    this.options = {
+      ...{encoding: 'utf8', trim: true, sync: true},
+      ...options,
+    };
+  }
+  
+  out(strings, ...values) {
+    const cmd = esc(strings, ...values);
+    if (this.options.sync) {
+      return sysExecSync(cmd, this.options);
+    } else {
+      return Promise.denodify(sysExec(cmd, this.options));
+    }
+  }
+  
+  p(strings, ...values) {
+    console.log(this.out(strings, ...values));
+  }
+}
+
+export function cmd(options) {
+  return new Cmd(options);
+}
+
+cmd.out = function(strings, ...values) {
+  return new Cmd().out(strings, ...values);
+}
+
+cmd.p = function(strings, ...values) {
+  return new Cmd().p(strings, ...values);
 }
