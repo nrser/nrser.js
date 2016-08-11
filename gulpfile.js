278417e78e358178250e71f2a254b98b2eb5cb36
nrser <neil@ztkae.com>
2016-08-11 19:43:36 +0800

    ok, think i have gulp task importing working now

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,3 @@
 const gulp = require('gulp');
-const _ = require('lodash');
 
-_.each(require('./lib/gulpTasks.js')().tasks, (args) => {
-  gulp.task.apply(gulp, args);
-});
+require('./lib/gulpTasks.js')(gulp)
