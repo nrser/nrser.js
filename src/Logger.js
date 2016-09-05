2ef5dc659796e8144cb60f485a95629b4cbb19ed
nrser <neil@ztkae.com>
2016-09-05 16:45:59 +0800

    just use _.cloneDeep to snapshot for now

--- src/Logger.js
+++ src/Logger.js
@@ -195,48 +195,54 @@ export class Logger {
     // Return a clone with the additive flags.
     return new RegExp(pattern, flags);
   } // cloneRegExp()
-  
-  static snapshot(value, seen = new Set()) {
-    if (Logger.canReferenceInSnaphot(value)) {
-      return value;
-      
-    } else if (_.isDate(value)) {
-      return new Date(value);
-      
-    } else if (_.isArray(value)) {
-      if (seen.has(value)) {
-        return value;
-      } else {
-        seen.add(value);
-        return _.map(value, function(i){ return Logger.snapshot(i, seen) });
-      }
-      
-    } else if (_.isRegExp(value)) {
-      return Logger.cloneRegExp(value);
+
+  static snapshot(value) {
+    const clone = _.cloneDeep(value);
     
-    // } else if (_.isError(value)) {
-    //   // TODO prob not right but how often to we mutate errors?
-    //   return value;
-      
-    } else if (_.isObject(value)) {
-      if (seen.has(value)) {
-        return value;
-      } else {
-        seen.add(value);
-        const copy = {};
-        // _.each(value, function(v, k) { copy[k] = Logger.snapshot(v, seen) });
-        for (let k in value) {
-          copy[k] = Logger.snapshot(value[k], seen);
-        }
-        return copy;
-      }
-      
-    } else {
-      console.log("snapshot() is not sure what this is", value);
-      
-      return `UNKNOWN: ${ value.toString() }`;
-    }
-  } // snapshot()
+    return clone;
+  }
+  
+  // static snapshot(value, seen = new Set()) {
+  //   if (Logger.canReferenceInSnaphot(value)) {
+  //     return value;
+  //     
+  //   } else if (_.isDate(value)) {
+  //     return new Date(value);
+  //     
+  //   } else if (_.isArray(value)) {
+  //     if (seen.has(value)) {
+  //       return value;
+  //     } else {
+  //       seen.add(value);
+  //       return _.map(value, function(i){ return Logger.snapshot(i, seen) });
+  //     }
+  //     
+  //   } else if (_.isRegExp(value)) {
+  //     return Logger.cloneRegExp(value);
+  //   
+  //   // } else if (_.isError(value)) {
+  //   //   // TODO prob not right but how often to we mutate errors?
+  //   //   return value;
+  //     
+  //   } else if (_.isObject(value)) {
+  //     if (seen.has(value)) {
+  //       return seen[value;
+  //     } else {
+  //       seen.add(value);
+  //       const copy = {};
+  //       // _.each(value, function(v, k) { copy[k] = Logger.snapshot(v, seen) });
+  //       for (let k in value) {
+  //         copy[k] = Logger.snapshot(value[k], seen);
+  //       }
+  //       return copy;
+  //     }
+  //     
+  //   } else {
+  //     console.log("snapshot() is not sure what this is", value);
+  //     
+  //     return `UNKNOWN: ${ value.toString() }`;
+  //   }
+  // } // snapshot()
   
   static setLevel({name, level}) {
     t.maybe(t.String)(name);
