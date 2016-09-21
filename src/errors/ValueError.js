549e4b9f0cec27294be1b75380333bac1da8e9de
nrser <neil@ztkae.com>
2016-09-21 14:36:31 +0800

    comments

--- src/errors/ValueError.js
+++ src/errors/ValueError.js
@@ -1,6 +1,7 @@
 import { BaseError } from './BaseError.js';
 
 /**
-* thrown when a value is not acceptable
+* thrown when a value is not acceptable (versus it being a TypeError if it's
+* the wrong type).
 */
 export class ValueError extends BaseError {}
