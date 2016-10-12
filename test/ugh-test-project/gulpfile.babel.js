c8c9e52babe4e167471619eb01b87b6574799c6d
nrser <neil@ztkae.com>
2016-10-12 20:58:46 +0800

    renamed ugh test project and added .gitignore

---
+++ test/ugh-test-project/gulpfile.babel.js
@@ -0,0 +1,12 @@
+import gulp from 'gulp';
+import Ugh from '../../src/ugh';
+
+const ugh = new Ugh(gulp);
+
+ugh.autoTasks();
+
+// ugh.less({
+//   name: 'index',
+//   src: 'src/ui/index.less',
+//   dest: 'public/css/index.css',
+// });
