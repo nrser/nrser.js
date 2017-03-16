236ae754c3cd111c12c746772e3d66fa9f13c835
nrser <neil@ztkae.com>
2017-03-16 21:56:11 +0800

    imrpove doc around env() and start appInstance() function

--- src/env.js
+++ src/env.js
@@ -47,7 +47,62 @@ export const IS_BROWSER = isBrowser();
 export const IS_NODE = isNode();
 
 /**
-* get the environment, defaulting to {@link DEFAULT_ENV}.
+* the *environment* dictates how things are built and behave
+* (versus "app instance", which declares *where* it's running / what it's
+* doing -- see {@link .appInstance}).
+* 
+* the environment can be set to anything by setting the `NODE_ENV` environment
+* variable, though unless you have given considerable through and uncovered 
+* sufficient reason, it should probably be one of:
+* 
+* 1.  `development` (default)
+* 2.  `production`
+* 3.  `test`
+* 
+* libraries and apps can of course respond to these values however they like,
+* but the general thematic should follow:
+* 
+* 1.  **`development`** (default)
+*     
+*     built and configured for development and debugging. this env
+*     should make it as easy as possible to work on the app, at the 
+*     expense of performance.
+*     
+*     things we expect in the development environment:
+*     
+*     -   lots of logging available.
+*         -   means [metalog][] is not stripping anything accept
+*             perhaps `trace` logging at compile time (via `.babelrc`
+*             config -- can still filter in runtime of course).
+*     -   quick and clear access to source code.
+*         -   no [minification][] or concatenation.
+*         -   source maps where necessary.
+*     -   tcomb type checking
+*         -   tcomb skips most checks in `production` mode by default, but it
+*             should be checking and throwing errors when things fail in 
+*             `development`.
+*     -   fail early and often
+*         -   while it makes sense to try and continue on in `production`, when
+*             in `development` anything unexpected should throw an error
+*             immediately.
+*    
+* 2.  **`production`**
+*     
+*     built and configured for performance.
+*     
+* 3.  **`test`**
+*     
+*     the environment set inside testing processes (mocha or whatever else).
+*     
+*     generally the same as `development`, with potential modifications to 
+*     support testing. 
+*     
+*     be aware that if testing frameworks are using ES2015+ source they will
+*     be doing the transformation under the `test` environment, and be sure
+*     to cover it in `.babelrc` if using `env` switching.
+* 
+* [minification]: https://en.wikipedia.org/wiki/Minification_(programming)
+* [metalog]: https://github.com/nrser/babel-plugin-metalog
 * 
 * @return {string}
 *   the environment name.
@@ -71,3 +126,17 @@ export function env() {
     
   }
 } // .env
+
+/**
+* the *app instance* is an identifier for *where* the app is deployed that 
+* build settings and runtime behavior can be dependent on. it is read from
+* the `NODE_APP_INSTANCE` environment variable on the server 
+* 
+* 
+* 
+* @return {?string}
+*   the app instance name (if any).
+*/
+export function appInstance() {
+  
+}
