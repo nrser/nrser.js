1e2459a22851c5c356ea3acd1692f8534521ed1e
nrser <neil@ztkae.com>
2017-06-02 12:44:21 +0800

    Mess with the log test scratch file

--- dev/scratch/log.js
+++ dev/scratch/log.js
@@ -1,9 +1,19 @@
 #!/usr/bin/env babel-node
 
-import '//lib/metalogger';
+import 'source-map-support/register';
 
-error: "Oh no!";
-warn: "Whoa there...";
-info: "BTW..."
-debug: "bug bug bug bug";
-trace: "blah blah blah";
+import * as NRSER from 'nrser';
+import 'nrser/lib/metalogger';
+
+function main() {
+
+  const error = new NRSER.NrserError('blah', {x: 1, y: "WHY?!?!"});
+
+  error: "Oh no!", error;
+  warn: "Whoa there...";
+  info: "BTW..."
+  debug: "bug bug bug bug";
+  trace: "blah blah blah";
+}
+
+main();
